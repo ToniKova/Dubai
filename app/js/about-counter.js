718e5147340d967d0d4cb9c407f4ec700b8910a5
Anton Anton <Toni95Kova@mail.com>
2023-04-24 18:10:45 +0300

    fix bag

--- app/js/about-counter.js
+++ app/js/about-counter.js
@@ -1,22 +1,38 @@
 
 
 const numCounter = document.querySelectorAll('.about__statistics-title')
+const about = document.querySelector('.about') //секция где находятся счетчики 
 
+let startAnimation = true
 
-let interval = 2000
-
-numCounter.forEach(item => {
-  let startValue = 0
-  let endValue = parseInt(item.getAttribute('data-count'))
-  let duration = Math.floor(interval / endValue)
-  let counter = setInterval(function() {
-    startValue += 1
-    item.textContent = startValue
-    if (startValue == endValue) {
-      clearInterval(counter)
-    }
-  },duration)
+window.addEventListener('scroll',() => {
+  let scrollTop = window.scrollY
+  let aboutTop = about.offsetHeight
+  if (scrollTop >= aboutTop) {
+    startCounter()
+  } 
 })
+
+function startCounter () {
+  if (startAnimation) {
+    let interval = 2000 //Время за которое будут работать счетчик
+    numCounter.forEach(item => {
+      let startValue = 0
+      let endValue = parseInt(item.getAttribute('data-count'))
+      let duration = Math.floor(interval / endValue)
+      let counter = setInterval(function() {
+        startValue += 1
+        item.textContent = startValue
+        if (startValue == endValue) {
+          clearInterval(counter)
+        }
+      },duration)
+    })
+    startAnimation = false 
+  } else {
+    
+  }
+}
 
 
 
--- app/js/swiper.js
+++ app/js/swiper.js
@@ -1,30 +1,54 @@
 
 
-const sliderOne = document.querySelector('.popular__slider-wrapper')
-const sliderTwo =  document.querySelector('.brand-slider__slider-wrapper')
+// const sliderOne = document.querySelector('.popular__slider-wrapper')
+// if (sliderOne) {
+//   let oneSlider = new Swiper(sliderOne, {
+//     slideClass : 'popular__slider-item',
+//     wrapperClass : 'popular__slider-body',
+
+//     direction: 'vertical',//Горизонтолая прокрутка
+//     spaceBetween: 15,// Отступы между слайдами
+//     initialSlide: 0,//С какого слайда показывать
+//     loop: true,//бесконечная прокрутка
+
+//     navigation : {
+//       nextEl: '.next',
+//       prevEl: '.prev'
+//     },
+//   })
+// }
+
+
+
+// const sliderTwo =  document.querySelector('.brand-slider__slider-wrapper')
+
+// if (sliderTwo) {
+//   let twoSlider = new Swiper(sliderTwo, {
+//     slideClass : 'brand-slider__slider-item',
+//     wrapperClass : 'brand-slider__slider-body',
+//   }) 
+// }
+
+
+
+// const sliderTwo = document.querySelector('.swiper')
+// if (swiperTwo) {
+//   let newSwiper = new Swiper(sliderTwo , {
+//   })
+// }
+
+const sliderTwo = document.querySelector('.swiper')
+
 
 if (sliderTwo) {
-  let twoSlider = new Swiper(sliderTwo, {
-    slideClass : 'brand-slider__slider-item',
-    wrapperClass : 'brand-slider__slider-body',
-  }) 
-}
-
-
-if (sliderOne) {
-  let oneSlider = new Swiper(sliderOne, {
-    slideClass : 'popular__slider-item',
-    wrapperClass : 'popular__slider-body',
-
-    direction: 'vertical',//Горизонтолая прокрутка
-    spaceBetween: 15,// Отступы между слайдами
-    initialSlide: 0,//С какого слайда показывать
-    loop: true,//бесконечная прокрутка
-
-
-    navigation : {
-      nextEl: '.next',
-      prevEl: '.prev'
-    },
+  let newSwiper = new Swiper(sliderTwo , {
+    loop: true, // Бесконечная прокрутка 
+    slideToClickedSlide: true, // Переключение по клику на слайдер 
+    spaceBetween: 100, // Растояние между слайдами 
+    slidesPerView : 'auto', // Сколько слайдом будет видно 
+    // loopedSlides: 3,
+    mousewheel: {
+      sensitivity: 1,
+    }
   })
 }
